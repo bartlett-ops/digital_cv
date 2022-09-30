43c6b60e19725181fecc5ccae7a94de31fd6fe63
Tom Bartlett <tom@bartlett.sh>
2022-09-30 20:44:53 +0800

    Renames
    Signed-off-by: Tom Bartlett <tom@bartlett.sh>

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,6 +1,6 @@
 module "ecr" {
   source                 = "./modules/ecr"
-  github_repository_name = "digital-cv"
+  github_repository_name = "digital_cv"
   providers = {
     // ECR public only available in us-east-1
     aws = aws.us_east_1
