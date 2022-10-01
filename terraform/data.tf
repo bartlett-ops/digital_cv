289492fe9bf4e4c43b5ea9626e9f21afd627452e
Tom Bartlett <tom@bartlett.sh>
2022-10-01 14:57:58 +0800

    Update iam permissions
    Signed-off-by: Tom Bartlett <tom@bartlett.sh>

--- terraform/data.tf
+++ terraform/data.tf
@@ -12,12 +12,25 @@ data "aws_iam_policy_document" "terraform_executor" {
 
 data "aws_iam_policy_document" "github_actions" {
   statement {
-    sid = "AuthenticatToECR"
+    sid = "AuthenticateWithECR"
     actions = [
       "ecr-public:GetAuthorizationToken",
       "sts:GetServiceBearerToken",
     ]
     resources = ["*"]
+  }
+  statement {
+    sid = "ECRPullPush"
+    actions = [
+      "ecr-public:CompleteLayerUpload",
+      "ecr-public:UploadLayerPart",
+      "ecr-public:InitiateLayerUpload",
+      "ecr-public:BatchCheckLayerAvailability",
+      "ecr-public:PutImage"
+    ]
+    resources = [
+      module.ecr.public_repository.arn
+    ]
   }
 }
 
